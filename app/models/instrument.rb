0202061e7505cedd196920d70a0291aaaa5ba2af
Michael Smethurst <michael@smethur.st>
2021-03-01 17:22:57 +0000

    updated fco to fcdo

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -5,7 +5,7 @@ class Instrument < ActiveRecord::Base
     description = description + self.title
     description = description + ' was laid on '
     description = description + self.date_laid.strftime( '%d-%m-%Y')
-    description = description + ' by the FCO, as '
+    description = description + ' by the FCDO, as '
     description = description + self.series
     description = description + '. Lead by the '
     description = description + self.lead_organisation
@@ -18,7 +18,7 @@ class Instrument < ActiveRecord::Base
     tweet_text = tweet_text + self.lead_organisation
     tweet_text = tweet_text + ' treaty '
     tweet_text = tweet_text + self.series
-    tweet_text = tweet_text + ' has been laid by the FCO '
+    tweet_text = tweet_text + ' has been laid by the FDCO '
     tweet_text = tweet_text + url
     tweet_text
   end
